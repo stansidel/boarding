4a2e587f279079d0806a3a3195d3f7c39e4dee32
Stan Sidel <stansidel@gmail.com>
2018-04-25 12:16:09 +0500

    Adds Qloud compatibility

--- config/puma.rb
+++ config/puma.rb
@@ -5,5 +5,5 @@ threads threads_count, threads_count
 preload_app!
 
 rackup      DefaultRackup
-port        ENV['PORT']     || 3000
+port        ENV['QLOUD_HTTP_PORT'] || ENV['PORT'] || 3000, '[::]'
 environment ENV['RACK_ENV'] || 'development'

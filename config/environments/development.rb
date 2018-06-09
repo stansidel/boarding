375e46e3115239f063472a43bebcc604b3b482cf
Stanislav Sidelnikov <stansidel@yandex-team.ru>
2018-06-09 12:12:26 +0500

    Changes logging to stdout

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,6 +17,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   # Print deprecation notices to the Rails logger.
+  config.logger = Logger.new(STDOUT)
   config.active_support.deprecation = :log
 
   # Debug mode disables concatenation and preprocessing of assets.
